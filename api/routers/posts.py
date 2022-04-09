acfb258b09a7b2e1740d43c59e5a9bb46b082dc0
LICHKING <savitarreus@gmail.com>
2022-04-09 15:50:06 +0300

    ❄️ Some changes ❄️

--- api/routers/posts.py
+++ api/routers/posts.py
@@ -5,6 +5,6 @@ from database import post_collection
 
 router = APIRouter(prefix="/posts", tags=["heroes"])
 
-@router.get("/{owner}", status_code=status.HTTP_200_OK)
-def get_post(owner: str) -> dict:
-    return post_collection.get_post({'owner': owner})
+@router.get("/", status_code=status.HTTP_200_OK)
+def get_post() -> dict:
+    return post_collection.get_all_posts()
